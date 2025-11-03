b86f4c3c477931c1c1a1f82c43899b1ea62fcf9b
GeorgeAngel <quispemescuag@gmail.com>
2025-11-03 15:00:18 -0500

    Creacion del Server y primera conexion con MongoDB

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
