b05af895d5407722d1fb92a1879b6ad5103373ba
GeorgeAngel <quispemescuag@gmail.com>
2025-11-03 18:36:30 -0500

    Testeando Auth Bearer

--- routes/testRoutes.js
+++ routes/testRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { testController } from "../Controllers/testController.js";
+import userAuth from "../Middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/test-post", testController);
+router.post("/test-post", userAuth, testController);
 
 export default router;
